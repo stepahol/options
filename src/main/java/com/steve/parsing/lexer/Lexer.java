dcffeafb634ea85859e3982c2ea3effee3e01caa
Stepan Kholopkin <sholopkin@c3g.ru>
2017-06-09 13:33:58 +0300

    finished with getFunctionArguments function testing

--- src/main/java/com/steve/parsing/lexer/Lexer.java
+++ src/main/java/com/steve/parsing/lexer/Lexer.java
@@ -10,6 +10,9 @@ import java.util.regex.Pattern;
  * Created by Степан on 08.06.2017.
  */
 public class Lexer {
+    public static final String ERR_EMPTY_ARG = "Error: there should no be any empty arguments";
+    public static final String ERR_NOT_A_FUNCTION = "Error: string is not a function";
+
     public static void parseString(String str) {
         String line = "seq(val(-d), val(-m))";
         FunctionArgsContainer container = getFunctionArguments(line);
@@ -20,20 +23,23 @@ public class Lexer {
     }
 
     public static FunctionArgsContainer getFunctionArguments(String line) {
-        String regex = "(\\w+)\\(([\\(\\),\\s\\w\\d-]+)\\)";
+        String regex = "^(\\w+)\\s*\\(([\\(\\),\\s\\w\\d-]*)\\)$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(line);
         if (matcher.find()) {
             String function = matcher.group(1);
             String args_str = matcher.group(2);
-            List<String> args = Arrays.asList(args_str.split(","));
+            List<String> args = Arrays.asList(args_str.split(",", -1));
             List<String> args_trimmed = new ArrayList<>();
             for (String arg : args) {
                 args_trimmed.add(arg.trim());
             }
+            if (args_trimmed.contains("")) {
+                throw new RuntimeException(ERR_EMPTY_ARG);
+            }
             return new FunctionArgsContainer(function, args_trimmed);
         } else {
-            return new FunctionArgsContainer();
+            throw new RuntimeException(ERR_NOT_A_FUNCTION);
         }
     }
 }
--- src/test/java/com/steve/parsing/lexer/LexerTest.java
+++ src/test/java/com/steve/parsing/lexer/LexerTest.java
@@ -1,14 +1,19 @@
 package com.steve.parsing.lexer;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by sholopkin on 09.06.2017.
  */
 public class LexerTest extends TestCase {
-    public void testGetFunctionArguments() {
+    @Test
+    public void testGetFunctionArguments_argWithoutNesting() {
         String line = "sum(a, b)";
 
         FunctionArgsContainer result = Lexer.getFunctionArguments(line);
@@ -19,5 +24,80 @@ public class LexerTest extends TestCase {
         assertEquals(resultArguments.size(), 2);
         assertEquals(resultArguments.get(0), "a");
         assertEquals(resultArguments.get(1), "b");
+    }
+
+    @Test
+    public void testGetFunctionArgument_argWithNesting1() {
+        String line = "sum(calc(a), b)";
+
+        FunctionArgsContainer result = Lexer.getFunctionArguments(line);
+        String resultFunction = result.getFunction();
+        List<String> resultArguments = result.getArguments();
+
+        assertEquals(resultFunction, "sum");
+        assertEquals(resultArguments.size(), 2);
+        assertEquals(resultArguments.get(0), "calc(a)");
+        assertEquals(resultArguments.get(1), "b");
+    }
+
+    @Test
+    public void testGetFunctionArgument_argWithNesting2() {
+        String line = "sum(a, val(b))";
+
+        FunctionArgsContainer result = Lexer.getFunctionArguments(line);
+        String resultFunction = result.getFunction();
+        List<String> resultArguments = result.getArguments();
+
+        assertEquals(resultFunction, "sum");
+        assertEquals(resultArguments.size(), 2);
+        assertEquals(resultArguments.get(0), "a");
+        assertEquals(resultArguments.get(1), "val(b)");
+
+
+    }
+
+    @Test
+    public void testGetFunctionArgument_argEmpty1() {
+        try {
+            String line = "sum(a, )";
+            Lexer.getFunctionArguments(line);
+            Assert.fail("No exception is thrown");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), Lexer.ERR_EMPTY_ARG);
+        }
+    }
+
+    @Test
+    public void testGetFunctionArgument_argEmpty2() {
+        try {
+            String line = "sum()";
+            Lexer.getFunctionArguments(line);
+            Assert.fail("No exception is thrown");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), Lexer.ERR_EMPTY_ARG);
+        }
+    }
+
+    @Test
+    public void testGetFunctionArgument_notAFunction() {
+        try {
+            String line = "sum";
+            Lexer.getFunctionArguments(line);
+            Assert.fail("No exception is thrown");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), Lexer.ERR_NOT_A_FUNCTION);
+        }
+    }
+
+    @Test
+    public void testGetFunctionArgument_parsesWithSpaces() {
+        String line = "sum     (    f (     a    ),  g(     b    )    )";
+        FunctionArgsContainer result = Lexer.getFunctionArguments(line);
+        String function = result.getFunction();
+        List<String> arguments = result.getArguments();
+        assertEquals(function, "sum");
+        assertEquals(arguments.size(), 2);
+        assertEquals(arguments.get(0), "f (     a    )");
+        assertEquals(arguments.get(1), "g(     b    )");
     }
 }
